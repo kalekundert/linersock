7c0c5631ff9f2d3511b7c460d22516b5b0393697
Kale Kundert <kale@thekunderts.net>
2015-11-14 11:37:53 -0800

    Add six as a dependency.

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ import distutils.core
 # $ python setup.py register -r pypi
 # $ python setup.py sdist upload -r pypi
 
-version = '1.1'
+version = '1.2'
 distutils.core.setup(
         name='linersock',
         version=version,
         author='Kale Kundert and Alex Mitchell',
-        packages=['linersock'],
         url='https://github.com/kxgames/linersock',
         download_url='https://github.com/kxgames/linersock/tarball/'+version,
         license='LICENSE.txt',
         description="A thin layer between you and your sockets that helps prevent chafing.",
         long_description=open('README.rst').read(),
-        keywords=['nonblocking', 'socket', 'wrapper', 'library'])
+        keywords=['nonblocking', 'socket', 'wrapper', 'library'],
+        packages=['linersock'],
+        install_requires=[
+            'six',
+        ],
+)
